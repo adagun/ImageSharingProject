886cc572525c63f6e57e2a2243768b8113446153
adagun <adagun@hotmail.com>
2021-09-01 10:46:56 +0200

    created post model

---
+++ ImageSharingProject/sharingapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=150, null=True, blank=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+    class Meta:
+        ordering = ['-uploaded']
