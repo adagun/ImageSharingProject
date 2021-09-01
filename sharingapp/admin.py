dbb45c261d099f045cab6bf00de9323be6a3285c
adagun <adagun@hotmail.com>
2021-09-01 12:33:55 +0200

    added postsview

--- sharingapp/admin.py
+++ sharingapp/admin.py
@@ -1,3 +1,11 @@
+from .models import Post
 from django.contrib import admin
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'uploaded', 'user']
+
+
+admin.site.register(Post, PostAdmin)
--- sharingapp/views.py
+++ sharingapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
+from .forms import PostForm
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+
+class PostsView(LoginRequiredMixin, ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "posts/posts.html"
+
+
+
