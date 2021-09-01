00448eaa17bd7e80f3099732ec7a1724db76c6d5
adagun <adagun@hotmail.com>
2021-09-01 13:01:38 +0200

    added create and edit post

--- sharingapp/urls.py
+++ sharingapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 
     path('posts/', views.PostsView.as_view(), name="posts"),
     path('', views.indexView, name="index"),
+    path('edit_post/<int:pk>/', views.PostEdit.as_view(), name="edit_post"),
+    path('delete_post/<int:pk>/', views.PostDelete.as_view(), name="delete_post"),
+    path('create_post/', views.PostCreate.as_view(), name="create_post"),
 ]
--- sharingapp/views.py
+++ sharingapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 from .forms import PostForm
@@ -9,12 +10,36 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
+def indexView(request):
+    return render(request, "index.html")
+
 
 class PostsView(LoginRequiredMixin, ListView):
     model = Post
     context_object_name = "posts"
     template_name = "posts/posts.html"
 
-def indexView(request):
-    return render(request, "index.html")
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('posts')
+    template_name = "posts/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreate, self).form_valid(form)
+
+
+class PostEdit(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('posts')
+    template_name = "posts/post_form.html"
+
+
+class PostDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = reverse_lazy('posts')
+    template_name = "posts/delete_post.html"
